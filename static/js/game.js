3f451d3bfd5ec861a351ee135ed446d7899c3cc5
JustinH11235 <61996677+JustinH11235@users.noreply.github.com>
2020-07-10 21:04:42 -0400

    Attempt fix mouse pos

--- static/js/game.js
+++ static/js/game.js
@@ -247,11 +247,13 @@ function sendCanvas(e) {
 }
 
 function setPosition(e) {
-    drawBuffer.push(true, e.clientX, e.clientY); // setPosition
+//     drawBuffer.push(true, e.clientX, e.clientY); // setPosition
+    drawBuffer.push(true, e.offsetX, e.offsetY); // setPosition
 }
 
 function setPosAndDotAndColorHistory(e) {
-    drawBuffer.push(true, e.clientX, e.clientY, false, e.clientX, e.clientY); // setPosition and dot
+//     drawBuffer.push(true, e.clientX, e.clientY, false, e.clientX, e.clientY); // setPosition and dot
+    drawBuffer.push(true, e.offsetX, e.offsetY, false, e.offsetX, e.offsetY); // setPosition and dot
 
     if (isDrawer) {
         updateColorHistory(hexToRGB(curColor.value));
@@ -262,76 +264,8 @@ function draw(e) {
     // mouse left button must be pressed
     if (e.buttons !== 1) return;
 
-    drawBuffer.push(false, e.clientX, e.clientY); // Line
+//     drawBuffer.push(false, e.clientX, e.clientY); // Line
+    drawBuffer.push(false, e.offsetX, e.offsetY); // Line
 }
 
 // End Drawing Handler
-
-
-
-
-
-
-
-
-
-
-
-// old code
-
-//   socket.on('disconnect', () => {
-//     console.log('Player Left Game');
-//     setTimeout(() => { window.location.replace('/') }, 1);
-//   });
-
-
-// // Start of Mobile Swipe Handler
-// document.addEventListener('touchstart', handleTouchStart, false);
-// document.addEventListener('touchmove', handleTouchMove, false);
-
-// var xDown = null;
-// var yDown = null;
-// function getTouches(evt) {
-//   return evt.touches ||             // browser API
-//     evt.originalEvent.touches; // jQuery
-// }
-
-// function handleTouchStart(evt) {
-//   const firstTouch = getTouches(evt)[0];
-//   xDown = firstTouch.clientX;
-//   yDown = firstTouch.clientY;
-// };
-
-// function handleTouchMove(evt) {
-//   if (!xDown || !yDown) {
-//     return;
-//   }
-
-//   var xUp = evt.touches[0].clientX;
-//   var yUp = evt.touches[0].clientY;
-
-//   var xDiff = xDown - xUp;
-//   var yDiff = yDown - yUp;
-
-//   if (Math.abs(xDiff) > Math.abs(yDiff)) {
-//     if (xDiff > 0) {
-//       /* left swipe */
-//       socket.emit('change direction', 'left');
-//     } else {
-//       /* right swipe */
-//       socket.emit('change direction', 'right');
-//     }
-//   } else {
-//     if (yDiff > 0) {
-//       /* up swipe */
-//       socket.emit('change direction', 'up');
-//     } else {
-//       /* down swipe */
-//       socket.emit('change direction', 'down');
-//     }
-//   }
-//   /* reset values */
-//   xDown = null;
-//   yDown = null;
-// };
-// // End Mobile Swipe Handler
